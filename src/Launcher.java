c53a4a0e4333f1c2e51fb8c52c9ad0dbfb080c8a
Brieuc <brieuc95130@gmail.com>
2021-12-01 12:02:26 +0100

    Commit pour la part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,21 +1,58 @@
 import java.util.Scanner;
 public class Launcher {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome !!!");
+    public static int fibo(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibo(n - 1) + fibo(n - 2);
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome ");
+
+        Scanner sc = new Scanner(System.in);
+        int resultat = 0;
         boolean a = true;
         while(a)
         {
+            boolean b =true;
             System.out.println("Rentrez un mot svp");
             String str = sc.nextLine();
-            if(str.equals("quit")){
-                break;
+            switch(str){
+
+                case "quit":
+                    b = false;
+                    break;
+
+                case "fibo":
+                    int nbre = 0;
+                    boolean c = true;
+                    while (c){
+                        System.out.println("Mettre une valeur a n svp");
+                        str = sc.nextLine();
+                        str = str.trim();
+                        try{
+                            nbre = Integer.parseInt(str);
+                            c = false;
+                        }
+                        catch(NumberFormatException nfe){
+
+                        }
+                    }
+                    resultat = fibo(nbre);
+                    System.out.println("le " + nbre + "ieme element de la suite est : " + resultat);
+
+                    b = false;
+                    break;
+
+                default:
+                    break;
             }
-            else {
-                System.out.println("Unknown command");
-                System.out.println("Rentrez un mot svp");
+            if(!b){
+                break;
             }
         }
     }
-}
